a24ef398d68f93763b47f5564bdabada933e5483
NicoH <nico.humpola@gmail.com>
2022-01-01 20:37:39 -0300

    Implementado el codigo en js y maquetando el archivo HTML

---
+++ Descuento.js
@@ -0,0 +1,18 @@
+
+function Discount (){
+
+    let precioOrigin = parseInt(document.getElementById ("precioOriginal").value);
+    let descuentoAplic = parseInt(document.getElementById ("descuentoAplicado").value);    
+
+    let precioFinal;
+
+    precioFinal = (precioOrigin * (100 - descuentoAplic)) / 100;
+
+    document.getElementById("precioFinal").innerText = `El precio final es: ${precioFinal}`;
+
+
+}
+
+
+
+
