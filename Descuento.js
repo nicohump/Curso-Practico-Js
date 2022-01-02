7ab8b4f85b4f48f202bf3001bacf5bd582ce3910
NicoH <nico.humpola@gmail.com>
2022-01-02 01:18:35 -0300

    Incorporacion de lista de cupones mediante el uso de objetos

--- Descuento.js
+++ Descuento.js
@@ -2,16 +2,28 @@
 function Discount (){
 
     let precioOrigin = parseInt(document.getElementById ("precioOriginal").value);
-    let descuentoAplic = parseInt(document.getElementById ("descuentoAplicado").value);    
+    let cuponSeleccionado = document.getElementById ("cupones").value;    
 
     let precioFinal;
+
+    const Cupones = {
+        miniDescuento : 10,
+        superDescuento : 25,
+        megaDescuento : 40
+    };
+
+    for (const prop in Cupones) {
+        if (prop == cuponSeleccionado) descuentoAplic = Cupones[prop]; 
+    }
 
     precioFinal = (precioOrigin * (100 - descuentoAplic)) / 100;
 
     document.getElementById("precioFinal").innerText = `El precio final es: $ ${precioFinal} `;
-
-
+    document.getElementById("descuentoAplicado").innerText = `El descuento aplicado es: ${descuentoAplic} %`;
 }
+
+
+
 
 
 
